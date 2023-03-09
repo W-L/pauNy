dfe316c6d45baa0db7041516d28efcf43014577c
W-L <weilguny.lukas@gmail.com>
2023-03-09 10:22:35 +0000

    add program location to sys path

--- pauNy/__init__.py
+++ pauNy/__init__.py
@@ -1,4 +1,7 @@
 import sys
+from pathlib import Path
+prog_path = Path(sys.argv[0]).parent
+sys.path.insert(0, f"{prog_path}/pauNy")
 sys.path.insert(0, "./pauNy")
 from lib import *
 from plot import *
