060b0e9c63082f351bea4f9bb06871fb0f866b83
W-L <weilguny.lukas@gmail.com>
2023-03-09 09:44:15 +0000

    adapt plotting tests to simpler interface

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -8,33 +8,29 @@ import pauny  # runscript import
 
 PATH_ALL = "data"
 REFPATH = "data/Saccharomyces_cerevisiae.R64-1-1.dna.toplevel.fa.gz"
-
+OUTNAME = "TESTING"
 
 
 class TestPlots(unittest.TestCase):
 
     def setUp(self):
         paths = pauny.parse_input_arg([PATH_ALL])
-        ac = pauNy.AssemblyCollection(paths=paths, reference_path=REFPATH)
-        nx, aun = ac.calculate_metrics()
-        nxf, aunf = ac.generate_dataframes(nx, aun)
-        self.nx_frame = nxf
-        self.aun_frame = aunf
-
+        self.ac = pauNy.AssemblyCollection(paths=paths, reference_path=REFPATH)
+        self.ac.calculate_metrics()
 
     def test_nx_plot(self):
-        p = pauNy.plot_nx(self.nx_frame)
+        p = pauNy.plot_nx(self.ac.nx_frame)
         self.assertTrue(isinstance(p, plotnine.ggplot))
 
     def test_aun_plot(self):
-        q = pauNy.plot_aun(self.aun_frame)
+        q = pauNy.plot_aun(self.ac.aun_frame)
         self.assertTrue(isinstance(q, plotnine.ggplot))
 
     def test_save_plot(self):
-        p = pauNy.plot_nx(self.nx_frame)
-        pauNy.save_plot(p, type="nx", out_name="testing", format="pdf")
-        check_exist = os.path.isfile('testing.nx.pdf')
-        check_notempty = os.path.getsize('testing.nx.pdf')
+        p = pauNy.plot_nx(self.ac.nx_frame)
+        pauNy.save_plot(p, type="nx", out_name=OUTNAME, format="pdf")
+        check_exist = os.path.isfile(f'{OUTNAME}.nx.pdf')
+        check_notempty = os.path.getsize(f'{OUTNAME}.nx.pdf')
         self.assertTrue(check_exist)
         self.assertTrue(check_notempty > 0)
 
