925fa86761983480888930ec472d1f4c6266a80d
W-L <weilguny.lukas@gmail.com>
2023-03-09 10:22:55 +0000

    adapt run script to simplified interface

--- pauny.py
+++ pauny.py
@@ -12,10 +12,10 @@ def setup_parser():
     parser = argparse.ArgumentParser(prog='pauNy',
                                      description='Nx curves and area under Nx in python')
     parser.add_argument('-i', '--input', dest='input', type=str, required=True, nargs='+',
-                        help='input fasta file(s) or director(ies) of files. Can be multiple (space-separated).')
+                        help='input fasta/fastq file(s) or director(ies) of files. Can be multiple (space-separated).')
     parser.add_argument('-o', '--out', dest='out', type=str, default="pauNy", help='base name for output files')
     reference_group = parser.add_mutually_exclusive_group(required=False)
-    reference_group.add_argument('-r', '--ref', dest='ref', type=str, default=None, help='path to reference fasta file')
+    reference_group.add_argument('-r', '--ref', dest='ref', type=str, default=None, help='path to reference sequence file')
     reference_group.add_argument('-g', '--genomesize', dest='genomesize', type=int, default=0, help='genome size or estimate')
     return parser
 
@@ -52,36 +52,23 @@ def main():
     args = parser.parse_args()
     # parse input file paths
     paths = parse_input_arg(args.input)
+
+
     # initialise a collection of assemblies from all input files
     asm = pauNy.AssemblyCollection(
         paths=paths,
         reference_path=args.ref,
-        genome_size=args.genomesize
+        genome_size=args.genomesize,
+        out_name=args.out,
     )
     # calculate Nx and auN values for all input files
-    nx_values, aun_values = asm.calculate_metrics()
-    # generate pandas frames from the values - passing is optional
-    nx_frame, aun_frame = asm.generate_dataframes(nx_values, aun_values)
-    # write dataframes to files - passing is optional
-    asm.metric_dataframes(
-        nx_frame=nx_frame,
-        aun_frame=aun_frame,
-        out_name=args.out
-    )
-    # generate and save plots - passing data frames is optional
-    asm.metric_plots(
-        nx_frame=nx_frame,
-        aun_frame=aun_frame,
-        out_name=args.out
-    )
-    # produce pngs for the example in repository
-    # asm.metric_plots(
-    #     nx_frame=nx_frame,
-    #     aun_frame=aun_frame,
-    #     out_name=args.out,
-    #     format="png"
-    # )
+    # this also generates pandas frames
+    asm.calculate_metrics()
+    # visualise results
+    asm.plot()
 
+    # produce pngs for the example in repository
+    # asm.plot(format="png")
 
 
 if __name__ == "__main__":
